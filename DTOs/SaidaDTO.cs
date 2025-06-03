ce5a7bbe627ee2d1a312c55b96321b87c8c8f395
Otavio <otavioaugustobr@gmail.com>
2025-06-03 02:50:39 -0300

    Criação da lógica da resposta e função auxiliar

---
+++ DTOs/SaidaDTO.cs
@@ -0,0 +1,33 @@
+using System.Text.Json.Serialization;
+
+namespace Saida
+{
+  public class SaidaJson
+  {
+    public int pedido_id { get; set; }
+
+    public Caixa caixas { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? observacao { get; set; }
+
+    public SaidaJson(int pedidoId, Caixa caixas)
+    {
+      pedido_id = pedidoId;
+      this.caixas = caixas;
+      observacao = null;
+    }
+  }
+
+  public class Caixa
+  {
+    public string caixa_id { get; set; }
+
+    public List<string> produtos { get; set; }
+    public Caixa(string id, List<string> prods)
+    {
+      caixa_id = id;
+      produtos = prods;
+    }
+  }
+}
