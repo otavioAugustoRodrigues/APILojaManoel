35e21dccfdbc7d0edad774ace44b95b7f3a88664
Otavio <otavioaugustobr@gmail.com>
2025-06-03 01:53:10 -0300

    Criação dos Models e teste de conexão

---
+++ Models/DimensoesModel.cs
@@ -0,0 +1,12 @@
+namespace Dimencao.Model
+{
+  public class Dimensoes
+  {
+    public int altura { get; set; }
+
+    public int largura { get; set; }
+
+    public int comprimento { get; set; }
+
+  }
+}
