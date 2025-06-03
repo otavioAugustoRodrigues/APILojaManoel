d201414897673deaf878b1774017eaf40b81fa52
Otavio <otavioaugustobr@gmail.com>
2025-06-03 02:50:39 -0300

    Criação da lógica da resposta e função auxiliar

--- Controllers/ProdutosController.cs
+++ Controllers/ProdutosController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using EntradaDTO;
+using Saida;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace Produtos.Controllers
 {
@@ -8,11 +10,34 @@ namespace Produtos.Controllers
   public class ProdutosController : ControllerBase
   {
     [HttpPost("teste")]
-    public IActionResult Teste([FromBody] Entrada Pedi)
+    public IActionResult Principal([FromBody] Entrada Pedi)
     {
-      var mensagens = Pedi.pedidos.Select(p => $"{p.pedido_id} é um dos pedidos").ToList();
+      var listOut = new List<SaidaJson>();
 
-      return Ok(new { mensagens });
+      foreach (var i in Pedi.pedidos)
+      {
+        int id_ped_novo = i.pedido_id;
+        string caixa_nova = $"Caixa {i.caixa}";
+
+        var listProd = new List<string>();
+        foreach (var p in i.produtos)
+        {
+          string prod = p.produto_id;
+          listProd.Add(prod);
+        }
+
+        var novaCaixa = new Caixa(
+          caixa_nova,
+          listProd);
+        var saidaItem = new SaidaJson(
+          id_ped_novo,
+          novaCaixa
+        );
+
+        listOut.Add(saidaItem);
+      }
+
+      return Ok(new { listOut });
     }
   }
 }
